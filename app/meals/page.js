2512c9ed0c2cf94da3a1c9f022f9c4c16dbb4448
KostovPV <106186518+KostovPV@users.noreply.github.com>
2024-11-02 11:24:10 +0200

    installed better-sqlite3 and database initialised, loading file created

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,8 +1,11 @@
 import Link from 'next/link';
 import classes from './page.module.css';
 import MealsGrid from '@/components/meals/meals-grid';
+import { getMeals } from '@/lib/meal';
 
-export default function MealsPage(params) {
+export default async function MealsPage(params) {
+    const meals = await getMeals();
+
     return (
         <>
             <header className={classes.header}>
@@ -18,7 +21,7 @@ export default function MealsPage(params) {
                 </p>
             </header>
             <main className={classes.main}>
-                    <MealsGrid meals={[]}/>
+                    <MealsGrid meals={meals}/>
             </main>
         </>
     )
